80970e512bd7508c52ce833dd28f7c9ec47416b2
PHU MAY THAW <phumay.25@ichat.sp.edu.sg>
2026-05-26 07:24:13 +0800

    Added return value

--- bmi.py
+++ bmi.py
@@ -6,8 +6,11 @@ def calculate_bmi(weight,height):
     print(f"BMI= {bmi:.2f}")
     if bmi<18.5:
         print("Underweight")
+        return -1
     elif bmi>25.0:
         print("Overweight")
+        return 1
     else:
         print("Normal weight")
+        return 0
 calculate_bmi(weight=57,height=1.73) 
